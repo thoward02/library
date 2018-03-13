65aeeaf328cf986dc29dc10b37b9058a07aa99b8
thoward02 <36546676+thoward02@users.noreply.github.com>
2018-03-13 12:16:18 -0400

    Fixed window(s) design
    The size of the window is restricted and colors were updated on the buttons.
    The python lesson main window was updated from the two pane design to the one pane design.

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const path = require('path');
 var mainWindow = null
 
 const createMainWindow = () => {
-  mainWindow = new BrowserWindow({width: 1000, height: 700, frame: false})
+  mainWindow = new BrowserWindow({width: 1000, height: 700, frame: false, resizable: false })
   console.log("[-- Started --]");
   mainWindow.loadURL(require('url').format({
     pathname: path.join(__dirname, 'main.html'), //Our main page where we're gonna load data etc etc
