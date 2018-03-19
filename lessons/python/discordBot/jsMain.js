f3e4ed5e5000a8d242e6aca7d3f91a0eacf181a5
JC TECH <33255294+JCTLearning@users.noreply.github.com>
2018-03-19 08:41:31 -0400

    Fix jsMain.js style basic, token, and async

--- lessons/python/discordBot/jsMain.js
+++ lessons/python/discordBot/jsMain.js
@@ -22,6 +22,7 @@ function main(){
     var lessonButton = document.createElement("button")
     lessonButton.id = lessons[items][1]
     lessonButton.innerHTML = lessons[items][0]
+    lessonButton.className = "buttons"
     document.getElementById("lessons").appendChild(lessonButton)
     //There is a billy joel concert on october the 13 in Salem NC.... I wonder if I can go, always did enjoy Piano Man
     //WHEN I WORE A YOUNGER MANS CLOTHES sorry im bored xd
